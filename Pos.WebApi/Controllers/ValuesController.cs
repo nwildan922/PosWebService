e787e87de9c801b8ec269a6d7315ab36f6004d47
wildan nugraha <nwildan922@gmail.com>
2018-05-22 23:01:24 +0700

    2nd commnit

--- Pos.WebApi/Controllers/ValuesController.cs
+++ Pos.WebApi/Controllers/ValuesController.cs
@@ -12,7 +12,7 @@ namespace Pos.WebApi.Controllers
         // GET api/values
         public IEnumerable<string> Get()
         {
-            return new string[] { "value1", "value2" };
+            return new string[] { "value1", "value 2" };
         }
 
         // GET api/values/5
